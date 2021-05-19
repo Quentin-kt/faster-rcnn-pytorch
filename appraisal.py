8eda4433ecc3dc4af5d33953420e02b5ae71e450
Quentin-kt <850027327@qq.com>
2021-05-19 20:45:19 +0800

    Update appraisal.py,更换classes

--- appraisal.py
+++ appraisal.py
@@ -4,8 +4,7 @@ import numpy as np
 import shutil
 
 # 初始化
-# classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog",
-#            "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["ChJ", "BX", "ZhAW", "CK", "QF", "TJ", "JG", "ShG", "PL", "ZhGAJ", "CQBG", "YWChR", "ShL", "FSh"]
 
 
 # 将文件夹内所有txt文件转换为一个tensor
